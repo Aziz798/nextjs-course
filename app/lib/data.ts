d5a70529c8f4cb08f00e380109df54dafd4c224d
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-04-06 20:20:46 +0100

    fixed deployment issues

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,11 +9,11 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-import {unstable_cache as noStore} from 'next/cache';
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
+ 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -33,7 +33,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
+  
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -54,7 +54,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore();
+  
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -126,7 +126,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -148,7 +148,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -174,7 +174,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore();
+
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -193,7 +193,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
